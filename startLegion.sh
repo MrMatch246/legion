8ccd202e362f77d9bcd2f8f9e8168d45a47adaee
MrMatch246 <daniel.erceg@aol.de>
2025-07-23 12:23:08 +0200

    path independent

--- startLegion.sh
+++ startLegion.sh
@@ -2,15 +2,16 @@
 
 echo "Strap yourself in, we're starting Legion..."
 
+REPO_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # Set everything we might need as executable
-chmod a+x -R ./deps/*
-chmod a+x -R ./scripts/*
+chmod a+x -R "$REPO_DIR/deps/*"
+chmod a+x -R "$REPO_DIR/scripts/*"
 
 # Determine OS, version and if WSL
-source ./deps/detectOs.sh
+source "$REPO_DIR/deps/detectOs.sh"
 
 # Determine and set the Python and Pip paths
-source ./deps/detectPython.sh
+source "$REPO_DIR/deps/detectPython.sh"
 
 # Figure if fist run or recloned and install deps 
 if [ ! -f ".initialized" ] | [ -f ".justcloned" ]
@@ -25,35 +26,35 @@ then
     if [ ! -z $ISWSL ]
     then
         echo "WSL Setup..."
-        bash ./deps/setupWsl.sh
+        bash "$REPO_DIR/deps/setupWsl.sh"
     fi
 
     # Install dependancies from package manager
     echo "Installing Packages from APT..."
-    ./deps/installDeps.sh
+    "$REPO_DIR/deps/deps/installDeps.sh"
 
     # Install python dependancies
     echo "Installing Python Libraries..."
-    ./deps/installPythonLibs.sh
+    "$REPO_DIR/deps/deps/installPythonLibs.sh"
 
     # Patch Qt
     echo "Stripping some ABIs from Qt libraries..."
-    ./deps/fixQt.sh
+    "$REPO_DIR/deps/deps/fixQt.sh"
 
     # Determine if additional Sparta scripts are installed
-    bash ./deps/detectScripts.sh
+    bash "$REPO_DIR/deps/deps/detectScripts.sh"
 
-    touch .initialized
-    rm .justcloned -f
+    touch "$REPO_DIR/.initialized"
+    rm "$REPO_DIR/.justcloned" -f
 fi
 
 export QT_XCB_NATIVE_PAINTING=0
 export QT_AUTO_SCREEN_SCALE_FACTOR=1.5
 
 # Verify X can be reached
-source ./deps/checkXserver.sh
+source "$REPO_DIR/deps/checkXserver.sh"
 
 if [[ $1 != 'setup' ]]
 then
-    /usr/bin/env python3 legion.py
+    /usr/bin/env python3 "$REPO_DIR/legion.py"
 fi
